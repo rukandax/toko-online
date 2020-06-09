d955e359a37eac7dc7cdecc35b2b9d43d3c19eba
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2020-06-09 12:44:59 +0700

    Update

--- index.php
+++ index.php
@@ -9,3 +9,6 @@ error_reporting($_ENV['ENVIRONTMENT'] == 'production' ? 0 : E_ALL);
 ini_set('display_errors', $_ENV['ENVIRONTMENT'] == 'production' ? 0 : 1);
 
 require_once __DIR__ . '/configs/database.php';
+
+$page = isset($_GET['page']) ? $_GET['page'] : 'landing';
+require_once __DIR__ . '/pages/' . $page . '/index.php';
