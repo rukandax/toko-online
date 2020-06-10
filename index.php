a2f5bcd8b16eaca6743ef3780df9854631978070
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2020-06-10 14:39:15 +0700

    Update 404

--- index.php
+++ index.php
@@ -11,4 +11,10 @@ ini_set('display_errors', $_ENV['ENVIRONTMENT'] == 'production' ? 0 : 1);
 require_once __DIR__ . '/configs/database.php';
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'landing';
-require_once __DIR__ . '/pages/' . $page . '/index.php';
+$file = __DIR__ . '/pages/' . $page . '/index.php';
+
+if (file_exists($file)) {
+    include_once $file;
+} else {
+    include_once __DIR__ . '/pages/notfound/index.php';
+}
